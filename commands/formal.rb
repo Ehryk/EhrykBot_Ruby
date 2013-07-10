2c08598eb1e5aeb980df7ade510826688a909523
Ehryk <rhaistlin@gmail.com>
2013-07-10 00:02:42 -0500

    formal now repeats the name before insulting

--- commands/formal.rb
+++ commands/formal.rb
@@ -3,9 +3,21 @@ require 'nokogiri'
 
 class Formal < Rubot::Core::Command
   def execute(server, message, options)
+
+  	n = 1
+	firstWord = message.body.split(/\s+/, n+1)[0...n].join(' ')
+	if ["me"].include? message.body.downcase
+		firstWord = message.from
+	end
+
+	reply = firstWord;
+	reply += ": "
 	
-	reply = Nokogiri::HTML(open('http://www.pangloss.com/seidel/Shaker/index.html?').read).xpath("//font//text()").to_s
-	
+	reply += Nokogiri::HTML(open('http://www.pangloss.com/seidel/Shaker/index.html?').read).xpath("//font//text()").to_s
+	reply = reply.gsub("\n", "")
+
+	p("Reply = " + reply)
+
     server.msg(message.destination, reply)
   end
 end
