67c1f08caf703a4254992ede275a4f058d440fa2
Ehryk <rhaistlin@gmail.com>
2013-10-28 03:46:37 -0500

    I forget.

--- commands/msg.rb
+++ commands/msg.rb
@@ -1,6 +1,15 @@
 class Msg < Rubot::Core::Command
   acts_as_protected
   def execute(server, message, options)
-    server.msg(message.body.split(" ")[0], message.body.split(" ")[1..-1].join(" "))
+  	channel = message.body.split(" ")[0]
+
+  	if channel.include? "#"
+  		reply = message.body.split(" ")[1..-1].join(" ")
+  	else
+  		channel = message.destination
+  		reply = message.body
+  	end
+
+    server.msg(channel, reply)
   end
 end
